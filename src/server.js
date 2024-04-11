0815f00c9ac9a47e1033fe4dc4d8e9476e0a8e42
huushuur <huushuur1052@gmail.com>
2024-04-11 09:18:36 +0900

    #3.3

--- src/server.js
+++ src/server.js
@@ -4,7 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+// 特定のルートへのHTTP GETリクエストに対する設定
+app.get("/", (req, res) => {
+  return res.send("I still love you");
+});
+app.get("/login", (req, res) => {
+  return res.send("Login here.");
+});
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT} 🚀`);
 
+// サーバーを起動してリクエストを待ち受ける
 app.listen(PORT, handleListening);
