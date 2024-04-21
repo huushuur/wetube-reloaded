93d1dc4c149b30fcb3d81894bf94ef2bef1987dc
huushuur <huushuur1052@gmail.com>
2024-04-21 22:39:50 +0900

    #6.13 Async Await

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,12 @@
 import Video from "../models/Video";
 
-export const home = (req, res) => {
-  Video.find({})
-    .then((videos) => {
-      console.log("videos", videos);
-      return res.render("home", { pageTitle: "Home", videos: videos });
-    })
-    .catch((error) => {
-      console.log("errors", error);
-    });
+export const home = async (req, res) => {
+  try {
+    const videos = await Video.find({});
+    return res.render("home", { pageTitle: "Home", videos });
+  } catch {
+    return res.render("server-error");
+  }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
