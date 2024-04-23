f306069b4674942ce585eb498cb61e19258018da
huushuur <huushuur1052@gmail.com>
2024-04-23 23:28:04 +0900

    #6.24 Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -30,11 +30,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) =>
-        word.startsWith("#") ? `#${word.replace(/#/g, "")}` : `#${word}`
-      ),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -49,7 +45,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
