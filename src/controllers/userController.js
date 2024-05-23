8ce0202691f4593e6c67573b0e9c23f76c37c75b
huushuur <huushuur1052@gmail.com>
2024-05-23 22:02:58 +0900

    #15.0 Installation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -173,11 +173,13 @@ export const postEdit = async (req, res) => {
 export const edit = (req, res) => res.send("Edit User");
 export const logout = (req, res) => {
   req.session.destroy();
+  req.flash("info", "Bye Bye");
   return res.redirect("/");
 };
 
 export const getChangePassword = (req, res) => {
   if (req.session.user.socialOnly === true) {
+    req.flash("error", "Can't change password.");
     return res.redirect("/");
   }
   return res.render("users/change-password", { pageTitle: "Change Password" });
@@ -206,7 +208,7 @@ export const postChangePassword = async (req, res) => {
   user.password = newPassword;
   await user.save();
   req.session.user.password = user.password;
-  // send notification
+  req.flash("info", "Password updated");
   return res.redirect("/users/logout");
 };
 
