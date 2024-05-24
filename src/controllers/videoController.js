1c216883ceb816b89ee2b3da426a032b654598ec
huushuur <huushuur1052@gmail.com>
2024-05-25 00:09:47 +0900

    #16.6 Rendering Comments

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,7 +10,7 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
   if (video === null) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
@@ -158,5 +158,14 @@ export const createComment = async (req, res) => {
     owner: user._id,
     video: id,
   });
+  video.comments.push(comment._id);
+  video.save();
+
+  const foundUser = await User.findById({ _id: user._id }).populate("comments");
+  if (!foundUser) {
+    return res.sendStatus(404);
+  }
+  foundUser.comments.push(comment._id);
+  foundUser.save();
   return res.sendStatus(201);
 };
