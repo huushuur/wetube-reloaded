8f97d1661f09e296b50025984e0fb8a073feeb4c
huushuur <huushuur1052@gmail.com>
2024-06-13 15:18:13 +0900

    Add classname

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -15,6 +15,7 @@ const addComment = (text, id) => {
   span.innerText = ` ${text}`;
   const span2 = document.createElement("span");
   span2.innerText = "❌";
+  span2.className = "video__comment-deleteBtn";
   span2.addEventListener("click", handleDelete);
   newComment.appendChild(icon);
   newComment.appendChild(span);
