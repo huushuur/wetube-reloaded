b09c007b37d96e77ce067b1efa3293fca3d8e2e5
huushuur <huushuur1052@gmail.com>
2024-06-15 00:00:07 +0900

    final!!!

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -145,6 +145,7 @@ export const postEdit = async (req, res) => {
     body: { name, email, username, location },
     file,
   } = req;
+
   const usernameExists =
     username != sessionUsername ? await User.exists({ username }) : undefined;
   const emailExists =
@@ -159,7 +160,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.path : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       name,
       email,
       username,
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,32 @@
 import multer from "multer";
+import { S3Client } from "@aws-sdk/client-s3";
+import multerS3 from "multer-s3";
+
+const s3Client = new S3Client({
+  region: "ap-northeast-1",
+  credentials: {
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const s3AvatarStorage = multerS3({
+  s3: s3Client,
+  bucket: "huushuur-wetube-fly-2024",
+  acl: "public-read",
+  key: function (req, file, cb) {
+    cb(null, `avatars/${req.session.user._id}/${Date.now().toString()}`);
+  },
+});
+
+const s3VideoStorage = multerS3({
+  s3: s3Client,
+  bucket: "huushuur-wetube-fly-2024",
+  acl: "public-read",
+  key: function (req, file, cb) {
+    cb(null, `videos/${req.session.user._id}/${Date.now().toString()}`);
+  },
+});
 
 export const localsMiddlewares = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -26,14 +54,14 @@ export const publicOnlyMiddleware = (req, res, next) => {
 };
 
 export const avatarUpload = multer({
-  dest: "uploads/avatars",
   limits: {
     fileSize: 3000000,
   },
+  storage: s3AvatarStorage,
 });
 export const videoUpload = multer({
-  dest: "uploads/videos",
   limits: {
     fileSize: 30000000,
   },
+  storage: s3VideoStorage,
 });
